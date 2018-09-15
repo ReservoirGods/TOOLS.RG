77d284fb67f60fb3d8ebb823172f975325677c47
pink-rg <leon_oreilly@yahoo.com>
2018-09-14 23:49:45 -0400

    updated testbed to support base tests

--- GOD_TEST/GOD_TEST.C
+++ GOD_TEST/GOD_TEST.C
@@ -1,9 +1,33 @@
+/* ###################################################################################
+#  INCLUDES
+################################################################################### */
+
 #include	<GODLIB\UNITTEST\UNITTEST.H>
 #include	<GODLIB\GEMDOS\GEMDOS.H>
 
+
+/* ###################################################################################
+#  LIST OF TESTS
+################################################################################### */
+
 #define	UNIT_TESTS_LIST					\
+	UNIT_TEST_ITEM( Base )				\
 	UNIT_TEST_ITEM( Encrypt )			\
 	UNIT_TEST_ITEM( LinkedList )
+
+
+/* ###################################################################################
+#  TEST SCAFFOLDING
+################################################################################### */
+
+
+/*	Prototypes of Unit test functions*/
+#define	UNIT_TEST_ITEM( _aName )  void UnitTest##_aName( void );
+UNIT_TESTS_LIST
+#undef	UNIT_TEST_ITEM
+
+
+/*	names and function pointers to unit tests*/
 
 typedef struct sUnitTestNamedFunctions
 {
@@ -11,16 +35,17 @@ typedef struct sUnitTestNamedFunctions
 	void( *mpFunction )( void );
 } sUnitTestNamedFunctions;
 
-#define	UNIT_TEST_ITEM( _aName )  void UnitTest##_aName( void );
-UNIT_TESTS_LIST
-#undef	UNIT_TEST_ITEM
-
 #define	UNIT_TEST_ITEM( _aName )  {	#_aName, UnitTest##_aName },
 struct sUnitTestNamedFunctions	gUnitTestNamedFunctions[] =
 {
 	UNIT_TESTS_LIST
 };
 #undef UNIT_TEST_ITEM
+
+
+/* ###################################################################################
+#  CODE
+################################################################################### */
 
 int		main( int argc, char **argv )
 {
@@ -30,7 +55,7 @@ int		main( int argc, char **argv )
 	printf( "GodLib Unit Tests\n" );
 	printf( "(c) 2018 Reservoir Gods\n\n" );
 
-
+	/* invoke unit tests*/
 #define	UNIT_TEST_ITEM( _aName )  UnitTest##_aName();
 	UNIT_TESTS_LIST
 #undef	UNIT_TEST_ITEM
