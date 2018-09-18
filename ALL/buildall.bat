da767422ce22eb766ef02e2c02324f22a4c8da78
cwmocean <cwmocean@yahoo.com>
2018-09-18 01:48:01 -0400

    clean up build all script

--- ALL/buildall.bat
+++ ALL/buildall.bat
@@ -1,22 +1,17 @@
 @echo off
-echo doing all buildall
+
 pushd ..\..
 
 pushd GODLIB.SPL
 call buildall.bat %*
-echo in godlibspl
-dir
 popd
-echo after popd1
-dir
+
 pushd TOOLS.RG
 echo in tools.rg
-dir
 call buildall %*
 popd
 
 pushd dev.rg
-dir
 call buildall %*
 popd
 
