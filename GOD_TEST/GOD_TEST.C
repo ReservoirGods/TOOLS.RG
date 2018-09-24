7fc959ca4df4305f3e2e95bbb834218e83d7776e
pink-rg <leon_oreilly@yahoo.com>
2018-09-24 00:25:22 -0400

    unit tests for string path functions

--- GOD_TEST/GOD_TEST.C
+++ GOD_TEST/GOD_TEST.C
@@ -14,7 +14,8 @@
 	UNIT_TEST_ITEM( Base )				\
 	UNIT_TEST_ITEM( Encrypt )			\
 	UNIT_TEST_ITEM( LinkedList )		\
-	UNIT_TEST_ITEM( Registry )
+	UNIT_TEST_ITEM( Registry )			\
+	UNIT_TEST_ITEM( String )
 
 
 /* ###################################################################################
